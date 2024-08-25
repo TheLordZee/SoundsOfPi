89d34a63da3713ff88e0f4c82e4f1235bcef7660
TheLordZee <l0rclz33@gmail.com>
2024-08-25 17:09:02 -0400

    pi to piano generator working

--- sounds.py
+++ sounds.py
@@ -6,29 +6,74 @@ from mingus.containers import Bar
 import mingus.core.chords as chords
 from mingus.containers import Track
 import mingus.core.notes as notes
-
+import mingus.extra.lilypond as LilyPond
 from notes_list import notes_list
 
-file = "pi.txt"
+source = "pi.txt"
 
-txt_file = open(file)
-
-count = 0
-
-
+file = open(source)
 t = Track()
 
-count = 0
+overflow = ""
+while True:
+    line = overflow + file.readline()
 
-bar = Bar()
+    if (not line) or len(overflow) == len(line):
+        break
 
-for key in notes_list.keys():
-    bar.place_notes(notes_list.get(key), 4)
-    count+=1
-    if(count == 4):
-        t.add_bar(bar)
-        bar = Bar()
-        count = 0
+    i = 0
+
+    while i < len(line) - 3:
+        num_notes = int(line[i])
+        i+=1
+        time = int(line[i:i+2]) if int(line[i:i+2]) > 0 else 100
+        i+=2
+        if num_notes == 0:
+            n=Note('D-7', dynamics={"channel":0})
+            if not t.add_notes(n, time):
+                curr_bar = t.bars[len(t.bars)-1]
+                beat =  curr_bar.length / (curr_bar.length - curr_bar.current_beat)
+                t.add_notes(n, beat)
+
+        elif i + (2 * num_notes) > len(line)-1:
+            i-=3
+            break
+        else:
+            curr_notes = []
+            while num_notes > 0:
+                curr_notes.append(notes_list.get(line[i:i+2]))
+                num_notes-=1
+                i+=2
+            if(not t.add_notes(curr_notes, time)):
+                curr_bar = t.bars[len(t.bars)-1]
+                beat =  curr_bar.length / (curr_bar.length - curr_bar.current_beat) 
+                t.add_notes(curr_notes, beat)
+                if(not t.add_notes(curr_notes, beat)):
+                    t.add_notes(curr_notes, beat+.0000000000000002)
+                    
+    overflow = line[i:len(line)-1]
+
+b = Bar()
+
+
+n=Note('D-7', dynamics={"channel":0})
+b.place_notes(n, 4)
+
+t.add_bar(b)
+# print(t)
+
+pond = LilyPond.from_Track(t)
+f = open("piSounds.txt", 'w')
+f.write(pond)
+# print(pond)
+LilyPond.to_pdf(pond, 'sheet')
+
+# i = 1
+# for bar in t.bars:
+#     pond = LilyPond.from_Bar(bar)
+#     LilyPond.to_png(pond, f'Bar{i}')
+#     LilyPond.to_pdf(pond, f'Bar{i}')
+#     i+=1
 
 p = Piano()
-p.play(t, recording_file="pi.wav", record_seconds=4)
+# p.play(t, recording_file="pi.wav")
